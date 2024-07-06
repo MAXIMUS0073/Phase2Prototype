69d42b9bdbc22f59055f0aeee2361cb61e865625
MAXIMUS0073 <149337480+MAXIMUS0073@users.noreply.github.com>
2024-07-06 02:35:14 -0500

    Update NavMakeAccount.java
    The user can now actually create an account. This happens by making a txt file that has all the contents that have been inputted except the password, which defaults to "password". The username for the patient will be generated here and be comprised of the first letter of the user's first name followed by his/her last name which is then followed by the last two digits of the year he/she was born in. If the user name generated already exists, nothing will occur and the message will appear in the console.

--- workplz/src/application/NavMakeAccount.java
+++ workplz/src/application/NavMakeAccount.java
@@ -35,14 +35,61 @@ public class NavMakeAccount
 	
     public void confirmButton(ActionEvent e) throws IOException
     {
-		System.out.println("Account created");
-		FXMLLoader loader = new FXMLLoader(getClass().getResource("patientPortal.fxml"));
-		Parent root = loader.load();
-		NavPatient NavPatient = loader.getController();
 		
-		stage = (Stage)((Node)e.getSource()).getScene().getWindow();
-		scene = new Scene(root);
-		stage.setScene(scene);
-		stage.show();
+		String firstName = firstNameField.getText().toLowerCase();
+		String lastName = lastNameField.getText().toLowerCase();
+		String DOB = DOBField.getText();
+		String email = emailField.getText().toLowerCase();
+		String phoneNumber = phoneNumberField.getText();
+		
+		String userName = firstName.substring(0,1) + lastName + DOB.substring(DOB.length() - 2);
+		//patient Pat = new patient(userName, firstName, lastName, DOB, email, phoneNumber, "password");
+		String filename = userName + "_PatientInfo.txt";
+    	
+    		if(doesFileExist(filename))
+    		{
+    			System.out.println("That username is already taken");
+    		}
+    		else
+    		{
+    			patient Pat = new patient(userName, firstName, lastName, DOB, email, phoneNumber, "password");
+    			savePatient(Pat);	//create file for patient
+	    
+			System.out.println("Account created");
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("patientPortal.fxml"));
+			Parent root = loader.load();
+			NavPatient NavPatient = loader.getController();
+		
+			stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+			scene = new Scene(root);
+			stage.setScene(scene);
+			stage.show();
+		}
+    }
+
+    private void savePatient(patient Pat)
+    {
+    	String filename = Pat.getUserName() + "_PatientInfo.txt";
+    	
+   		try (PrintWriter writer = new PrintWriter(filename, "UTF-8"))
+   		{
+   			writer.println("Username: " + Pat.getUserName());
+   			writer.println("First Name: " + Pat.getFirstName());
+   			writer.println("Last Name: " + Pat.getLastName());
+   			writer.println("DOB: " + Pat.getDOB());
+   			writer.println("Email: " + Pat.getEmail());
+    			writer.println("Phone Number: " + Pat.getPhoneNumber());
+    			writer.println("Password: " + Pat.getPassword());
+    		}
+    		catch (IOException e)
+    		{
+    			System.out.println("An error occurred while trying to save the patient information.");
+    			e.printStackTrace();
+    		}
+    }
+	
+    private boolean doesFileExist(String filename)
+    {
+    	return Files.exists(Paths.get(filename));
     }
 }
